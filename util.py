fad76125a1438580da9e77960d3a7411d61e23b1
Fruzsina Kiss <fruzsinakiss1@gmail.com>
2019-03-18 19:01:45 +0100

    timestamp function implemented that generated timestamp for new question

--- util.py
+++ util.py
@@ -1,6 +1,12 @@
 import connection
+import time
 
 def generate_id(filename):
     max_id = len(connection.csv_reader(filename))
     generated_id = max_id + 1
     return generated_id
+
+def get_current_time():
+    timestamp = time.time()
+
+    return timestamp
