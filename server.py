d84dcc5d68814ae1dfe79c3f54287a2779e076c6
Fruzsina Kiss <fruzsinakiss1@gmail.com>
2019-03-19 13:32:17 +0100

    all good good

--- server.py
+++ server.py
@@ -9,18 +9,22 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def list_questions():
-    questions = data_manager.get_all_questions()
-    sorted_questions = util.sort_questions_by_timestamp()
-    return render_template("list_questions.html", questions=sorted_questions)
+    sorted_questions_by_timestamp = util.sort_questions_by_timestamp()
+    sorted_questions_date_format = util.get_date_format_for_sorted_data(sorted_questions_by_timestamp)
+
+    return render_template("list_questions.html", questions=sorted_questions_date_format)
 
 
 @app.route('/question/<question_id>')
 def display_question(question_id: int):
-    answers = data_manager.get_all_answers()
-    sorted_anwers = util.sort_answers_by_timestamp()
-    questions = data_manager.get_all_questions()
 
-    return render_template("display_question.html", answers=sorted_anwers, questions=questions, question_id=question_id)
+    sorted_anwers = util.sort_answers_by_timestamp()
+    sorted_answers_date_format = util.get_date_format_for_sorted_data(sorted_anwers)
+
+    sorted_questions = util.sort_questions_by_timestamp()
+    sorted_questions_date_format = util.get_date_format_for_sorted_data(sorted_questions)
+
+    return render_template("display_question.html", answers=sorted_answers_date_format, questions=sorted_questions_date_format, question_id=question_id)
 
 
 @app.route("/add-question", methods=["GET"])
--- util.py
+++ util.py
@@ -1,12 +1,15 @@
 import connection
+from datetime import datetime
 import time
 import data_manager
 
 
 def get_current_time():
     timestamp = time.time()
+    timestamp = round(timestamp)
 
     return timestamp
+
 
 def sort_questions_by_timestamp():
     data = connection.read_data_from_file(connection.QUESTIONS_PATH)
@@ -21,3 +24,12 @@ def sort_answers_by_timestamp():
     sorted_data = sorted(data, key=lambda k: k["submission_time"], reverse=True)
 
     return sorted_data
+
+def get_date_format_for_sorted_data(data):
+
+    for row in data:
+        timestamp = int(row["submission_time"])
+        row["submission_time"] = datetime.utcfromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M")
+
+    return data
+
